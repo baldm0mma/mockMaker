90b2c5dd5d7b57a7c7a4ac22bf0c4f8548fefc57
jev forsberg <jev.forsberg@grafana.com>
2024-03-01 16:18:02 -0700

    main/ add more logic

--- src/propsGen.ts
+++ src/propsGen.ts
@@ -1,9 +1,16 @@
 import { program } from "commander";
-// import ts from "typescript";
-// import { readFileSync } from "fs";
-// import { parse } from "react-docgen";
-// const reactDocgen = import("react-docgen");
-import * as reactDocgen from "react-docgen-typescript";
+// import * as reactDocgen from "react-docgen-typescript";
+import {
+  createSourceFile,
+  ScriptTarget,
+  isTypeAliasDeclaration,
+  forEachChild,
+  isImportDeclaration,
+  resolveModuleName,
+  isStringLiteral,
+  sys,
+} from "typescript";
+import { readFileSync } from "fs";
 
 // Parse the command line arguments
 program.parse(process.argv);
@@ -11,16 +18,89 @@ program.parse(process.argv);
 // Access the arguments directly
 const [componentName, componentPath] = program.args;
 
-export const generateProps = async () => {
-  // Read component file
-  // const fileContent = readFileSync(componentPath, "utf8");
-  // console.log(fileContent, "fileContent");
+// export const generateProps = async () => {
+//   // const result = reactDocgen.parse(componentPath);
+//   // console.log(result[0].props.content.declarations, "propTypeResults");
+//   // // console.log(`name: ${componentName}, path: ${componentPath}`);
+//   // const sourceFile = ts.createSourceFile(
+//   //   componentName,
+//   //   readFileSync(componentPath, "utf8"),
+//   //   ts.ScriptTarget.Latest
+//   // );
 
-  const parser = reactDocgen.parse;
-  const result = parser(componentPath);
-  console.log(result[0].props, "propTypeResults");
+//   // // @ts-ignore
+//   // console.log("sourceFile: ", sourceFile);
 
-  // console.log(`name: ${componentName}, path: ${componentPath}`);
-};
+//   // if (
+//   //   ts.isInterfaceDeclaration(sourceFile) ||
+//   //   ts.isTypeAliasDeclaration(sourceFile)
+//   // ) {
+//   //   console.log("Declared type: ", sourceFile);
+//   //   // You can do more processing based on the type information if needed
+//   // }
+// };
+function parseTypescriptFile(filePath: string, parsedFiles = new Set()) {
+  // Check if the file has already been parsed to avoid infinite loops for circular dependencies
+  if (parsedFiles.has(filePath)) {
+    return [];
+  }
 
-generateProps();
+  parsedFiles.add(filePath);
+
+  const fileContent = readFileSync(filePath, "utf8");
+
+  const sourceFile = createSourceFile(
+    filePath,
+    fileContent,
+    ScriptTarget.Latest,
+    true
+  );
+
+  const typeDefinitions: any = [];
+
+  function visit(node: any) {
+    if (isImportDeclaration(node) && isStringLiteral(node.moduleSpecifier)) {
+      const importFilePath = getImportedFilePath(
+        node.moduleSpecifier.text,
+        filePath
+      );
+
+      if (!importFilePath) {
+        const importedTypeDefinitions = parseTypescriptFile(
+          // @ts-ignore
+          importFilePath,
+          parsedFiles
+        );
+        typeDefinitions.push(...importedTypeDefinitions);
+      }
+    }
+
+    if (isTypeAliasDeclaration(node)) {
+      typeDefinitions.push({
+        name: node.name.escapedText,
+        type: node.type.getText(),
+      });
+    }
+
+    forEachChild(node, visit);
+  }
+
+  function getImportedFilePath(moduleSpecifier: any, currentFilePath: any) {
+    const resolvedModule = resolveModuleName(
+      moduleSpecifier,
+      currentFilePath,
+      {},
+      sys
+    ).resolvedModule;
+
+    // Check if resolvedModule is defined and has a resolvedFileName
+    return (resolvedModule && resolvedModule.resolvedFileName) || null;
+  }
+
+  visit(sourceFile);
+
+  console.log(typeDefinitions, "typeDefinitions");
+  return typeDefinitions;
+}
+
+parseTypescriptFile(componentPath);
