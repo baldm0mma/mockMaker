80ee18a12075ba1890fdc7873cbf6c37a6b16ae6
jev forsberg <jev.forsberg@grafana.com>
2024-03-01 11:56:03 -0700

    main/ transpile

---
+++ src/propsGen.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateProps = void 0;
+var generateProps = function () {
+    return "hello world!";
+};
+exports.generateProps = generateProps;
