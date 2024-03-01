18d116e83ff42778528d0345cde474fc10ae3322
jev forsberg <jev.forsberg@grafana.com>
2024-03-01 13:42:50 -0700

    main/ update package

--- src/propsGen.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateProps = void 0;
-var generateProps = function () {
-    console.log("hello world!");
-};
-exports.generateProps = generateProps;
