a562204fb176109a73daf2f3a8642c37b7411d63
jev forsberg <jev.forsberg@grafana.com>
2024-03-01 16:18:12 -0700

    main/ add props

--- tests/App.tsx
+++ tests/App.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Button, ButtonProps } from "./Button";
+
 type AppProps = {
   id: number;
   name: string;
@@ -9,6 +12,15 @@ type Content = {
   body: string;
 };
 
+const buttonProps: ButtonProps = {
+  onButtonClick: () => undefined,
+  color: "blue",
+  text: "imma button",
+};
+
 export const App = (_props: AppProps) => {
-  return "App";
+  const { onButtonClick, color, text } = buttonProps;
+  return (
+    <Button onButtonClick={onButtonClick} color="blue" text="imma button" />
+  );
 };
